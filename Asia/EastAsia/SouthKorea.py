8b31bd2e34cbce188fdff526af221730695b37f8
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2026-08-16 11:08:23 +0000

    Updated South Korea

--- Asia/EastAsia/SouthKorea.py
+++ Asia/EastAsia/SouthKorea.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 parties = ['PPP', 'DPK', 'RKP', 'RP', 'PP']
-polling_2025 = [23.7, 49.9, 3.5, 3.6, 1.1]
+polling_2026 = [21.0, 40.0, 2.0, 2.0, 1.0]
 election_2024 = [36.67, 26.70, 24.25, 3.62, 0.10]
 
 poll_colors = ['#e30424', '#152481', '#2773b8', '#e96d08', '#d4041c']
@@ -13,7 +13,7 @@ width = 0.6
 
 fig, ax = plt.subplots(figsize=(10, 8))
 ax.bar(x + width/10, election_2024, width=width, color=election_colors, label='2024 Election')
-ax.bar(x - width/10, polling_2025, width=width, color=poll_colors, label='August 2025 Polling')
+ax.bar(x - width/10, polling_2026, width=width, color=poll_colors, label='August 2026 Polling')
 
 ax.set_ylabel('%')
 ax.set_xticks(x)
